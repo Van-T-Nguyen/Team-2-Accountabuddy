5201b00be9c0e22b9ce4644f300e7878d9545155
Rubicon7 <56270335+Rubicon7@users.noreply.github.com>
2020-09-19 21:28:42 -0500

    Update queueup.py

--- queueup.py
+++ queueup.py
@@ -26,9 +26,13 @@ class QueueCog(commands.Cog):
         """Returns true if a user is in a specific server. False otherwise."""
         
         guild = await self.bot.fetch_guild(guildid) 
-        result = guild.get_member(userid)
-        if(result != None): #Succeded
+        members = guild.members
+        target = await guild.fetch_member(userid)
+        print("call details: userid: {}".format(userid))
+        print("target: {}".format(target))
+        if(target.id == userid):
             return True
+        print("Failed loop: Members:\n{}\n\nGuild:\n{}".format(members,guild))
         return False
     
     @commands.command()
@@ -52,19 +56,58 @@ class QueueCog(commands.Cog):
         for interest in interests:
             interestlist += interest+'\n'
         
-        statusmessage = await ctx.send("Possible interests:\n{}\nPlease enter one or a few!".format(interestlist))
+        statusmessage = await ctx.send("Hi! I'm Accountabuddy. Select one or a few areas you want to be held accountable for and I'll work to pair you with someone also wanting to be held accountable for the same thing.\n\nAvailable communities:\n{}\nPlease enter one or a few!".format(interestlist))
         
+        tries = 2
         
-        #This block waits for a message reply.
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        try:
-            newmsg = await self.bot.wait_for('message', check=check,timeout=60)
-        except asyncio.TimeoutError:
-            await statusmessage.edit(content="Timed out.")
-            return
+        while tries != 0:
+            tries -= 1
         
-        statusmessage.edit(content="Parsed message: {}".format(newmsg.content))
+            #This block waits for a message reply.
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+            try:
+                newmsg = await self.bot.wait_for('message', check=check,timeout=60)
+            except asyncio.TimeoutError:
+                await statusmessage.edit(content="Timed out waiting for a reply.")
+                tries = 0
+                return
+        
+            #await statusmessage.edit(content="Parsed message: {}".format(newmsg.content))
+            
+            #Search the response for interests.
+            
+            readinterests = [] #List of found interests in message
+            words = newmsg.content.lower()
+            print("words: {}".format(words))
+            
+            for interest in interests:
+                if(interest.lower() in words ): #Inside the list of interests, all lowercase
+                    readinterests.append(interest)
+            
+            if(len(readinterests)==0):#Couldn't parse any interests
+                
+                if(tries!=0):
+                    await ctx.send("Could you try that again? I couldn't pick up anything you said from the list.")
+                else: #Give up
+                    await ctx.send("I couldn't follow you at all... Try again later, mayhaps?")
+                    return
+                continue
+            else:
+                break
+            
+            
+        
+        if(tries==0)
+        
+        debugtext = "Parsed interests: {}\n".format(readinterests)
+        
+        debugtext += "Relations:\n"
+        for read in readinterests:
+            debugtext += "{}: {}\n".format(read,kvGetValue(interestsfile,read).split("$"))
+        
+        await ctx.send(content=debugtext)
+        
         
         return
         
@@ -86,7 +129,7 @@ class QueueCog(commands.Cog):
         
         pass
     
-    
+    @tasks.loop(seconds=60.0) #This runs every minute.
     async def queueUpdate(self):
         """Check for pairs and pair them if applicable."""
         #Check for possible pairings in the file.
