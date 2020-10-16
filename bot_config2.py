892305c7aa60b1e2b0aea85a49cbbdb314169cb2
Ubuntu <ubuntu@ip-172-31-27-66.us-east-2.compute.internal>
2020-10-16 19:38:14 +0000

    hey

--- bot_config2.py
+++
@@ -1,9 +0,0 @@
-
-token = "NzU0ODQxNDI3MTQxMzI4OTM2.X16m3A.QDw9ybiHSjREIi1NsDoAB_9yF_k"  #The token the bot controls.
-#token = "NjYxNzQ2ODA0ODE1Mjk4NTgw.Xgv5vA.VFWVqL-wC773x2Z0HgSnnmWh8k0" #This is Menacing's token, it works great
-
-DM_Channel = 753122017334526043 #DMs received by the bot will be forwarded here.
-
-Home_Server = 751136700029206558 #ID of our "home" server. Currently this is Accountabuddy Universe.
-
-Invite_To_Home_Server = "https://discord.gg/gWuJJCm" #Invite given to those not in the home server.
