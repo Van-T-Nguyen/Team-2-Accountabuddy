2600269a9116d8072abb0dea30d690c0658e8a5e
randy hartzell <rwh9609@gmail.com>
2020-10-24 15:25:10 -0500

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,5 +1,5 @@
 
-token = "NzUzNzgyNTY0NzQ1MTE3Nzk3.X1rMuA.aboTT8YNebHXcIHVc1YfIawObuA"
+token = "NzY5NjUwMzAzNDAxODUyOTI4.X5SGsw.YOPldmCBh65eBKGo7fOhpp9QseU"
 
 DM_Channel = 753122017334526043 #DMs received by the bot will be forwarded here.
 
@@ -7,4 +7,4 @@ Home_Server = 751136700029206558 #ID of our "home" server. Currently this is Acc
 
 Invite_To_Home_Server = "https://discord.gg/gWuJJCm" #Invite given to those not in the home server.
 
-pfix = "!v" #Changeable prefix for calling the bot.
+pfix = "!randy" #Changeable prefix for calling the bot.
