7d931737aa00778c0986618d7266a0ac8e468591
Ubuntu <ubuntu@ip-172-31-47-158.us-east-2.compute.internal>
2021-04-21 01:26:42 +0000

    leaderboard update

--- leaderboard.py
+++ leaderboard.py
@@ -13,30 +13,35 @@ from quickstart import *
 
 spread = spread()
 
+
+
 class leaderboard(commands.Cog):
 
-	def __init__(self, bot):
-		self.bot = bot
-		self.homeserver = bot_config.Home_Server
-		self.leaderboardMsg = None
-		self.channel = self.bot.get_channel(826526139756969984)
-		self.updateLeaderboard.start()
+    def __init__(self, bot):
+        self.bot = bot
+        self.homeserver = bot_config.Home_Server
+        self.leaderboardMsg = None
+        self.channel = self.bot.get_channel(826526139756969984)
+        self.updateLeaderboard.start()
 
-	def createLeaderboard(self):
-		sortSheet(spread, "Leaderboard", 2, "DESCENDING")
-		ids = []
-		names = []
-		scores = []
-		ids, names, scores = get_sheet(spread, "Leaderboard")
-		lb = "First: {}: {}\nSecond: {}: {}\nThird: {}: {}".format(names[0], scores[0], names[1], scores[1], names[2], scores[2])
-		return lb
+    def createLeaderboard(self):
+        sortSheet(spread, "Leaderboard", 2, "DESCENDING")
+        ids = []
+        names = []
+        scores = []
+        ids, names, scores = get_sheet(spread, "Leaderboard")
+        lb = "**Top Accountable Users:**\n"
+        for i in range(len(scores)):
+            lb += "**{}**: {}\t\t{}\n".format(i+1,names[i],scores[i]);
+        #lb = "First: {}: {}\nSecond: {}: {}\nThird: {}: {}".format(names[0], scores[0], names[1], scores[1], names[2], scores[2])
+        return lb
 
-	@tasks.loop(seconds=30)
-	async def updateLeaderboard(self):
-		if(self.leaderboardMsg == None):
-			self.leaderboardMsg = await self.channel.send(self.createLeaderboard())
-		else:
-			await self.leaderboardMsg.edit(content=self.createLeaderboard())
+    @tasks.loop(seconds=30)
+    async def updateLeaderboard(self):
+        if(self.leaderboardMsg == None):
+            self.leaderboardMsg = await self.channel.send(self.createLeaderboard())
+        else:
+            await self.leaderboardMsg.edit(content=self.createLeaderboard())
 
 def setup(bot):
-	bot.add_cog(leaderboard(bot))
+    bot.add_cog(leaderboard(bot))
